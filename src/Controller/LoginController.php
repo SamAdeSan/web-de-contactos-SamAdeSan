fa50e6ae162ae0caf55b06863cd87c5736a788b5
SamAdeSan <samadesan@alu.edu.gva.es>
2025-10-16 08:18:46 +0000

    Archivos modificados

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -19,7 +19,7 @@ final class LoginController extends AbstractController
 		return $this->render('login/index.html.twig', [
             'controller_name' => 'LoginController',
             'last_username' => $lastUsername,
-            'error'         => $error,
+            'error' => $error,
           ]);
     }
 }
--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -6,7 +6,6 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class SecurityController extends AbstractController
 {
-
     #[Route('/logout', name: 'app_logout', methods:["GET"])]
     public function logout(): void
     {
